1994bdfe3998c6c4fbe9444c0f5c9b54b17f2768
Rish <akshaw05@aol.com>
2023-05-26 23:00:01 +0530

    Lists

---
+++ Lists.py
@@ -0,0 +1,28 @@
+#List of an integer type
+
+numbers = [32,54,77,43,22]
+print(numbers) #It prints all elements in list
+print(numbers[2]) #It Print specific element
+print(numbers[0:3]) #It Print elements in range
+
+
+#List of a String type
+
+names = ["rishu","anshu","piyush","aman"]
+print(names) 
+names.append("Navin ul Haque")  # Adding element to list
+print(names)
+
+print(names[0]) 
+
+#Creating an empty list
+
+emp = []
+print(emp)
+
+
+#List with multiple type values
+
+mixed = [133,534,"Surya",3444,"Vijay",57.4]
+print(mixed)
+
