5707aa0add63b4e25e1af00feef3c4067ba9bda1
Rish <akshaw05@aol.com>
2023-05-29 12:48:54 +0530

    String fomatting %s , f{} , str.format()

--- Strting_Formatting.py
+++ Strting_Formatting.py
@@ -1,13 +1,34 @@
-name  = "Rishu"
-print("My name is %s!"%name)
+name  = "Rishu"   #%s
+print("My name is %s!"%name)  #%s
 print("%s is a good boy"%name)
 
 inr = 20
 print("The cost price of pen is ₹%d."%inr)
 print("The cost price of pen is ₹",20,".")
 
+age = int(input("Enter your age :"))
+message = "My name is {} and I am {} years old.".format(name,age)
+print(message)
+
+col1 = input("Enter color name :") 
+col2 = input("Enter color name :")
+
+print("My favourite color is {} and {}.".format(col1,col2))
+print("My favourite color is {col2} and {col1}.".format(col1="red",col2="yellow"))
+print("My favourite color is {col1} and {col2}.".format(col1="yellow",col2="red"))
+
+print(f"My name is {name}.")
+
+message1 = f"My name is {name} and I am {age} years old." #f{}
+print(message1)
+message1 = "My name is {name} and I am {age} years old.".format(name="Rishu",age=16) #str.format()
+print(message1)
 
 '''
+Three ways of formatting :
+1.%s
+2.str.format()
+3.f{}
 String Formatting
 
 %s - String (or any object with a string representation, like numbers)
