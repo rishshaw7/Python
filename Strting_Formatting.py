f614fa804f0887b899348e30da3acbbc201b2f9a
Rish <akshaw05@aol.com>
2023-05-29 12:19:41 +0530

    String and integer formatting

---
+++ Strting_Formatting.py
@@ -0,0 +1,22 @@
+name  = "Rishu"
+print("My name is %s!"%name)
+print("%s is a good boy"%name)
+
+inr = 20
+print("The cost price of pen is ₹%d."%inr)
+print("The cost price of pen is ₹",20,".")
+
+
+'''
+String Formatting
+
+%s - String (or any object with a string representation, like numbers)
+
+%d - Integers
+
+%f - Floating point numbers
+
+%.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+
+%x/%X - Integers in hex representation (lowercase/uppercase)
+'''
