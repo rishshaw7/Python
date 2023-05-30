167b884e53af0ae412782959fe85c19da2814060
Rish <akshaw05@aol.com>
2023-05-30 19:29:09 +0530

    String slicing negative and positive slicing

--- String_Methods.py
+++ String_Methods.py
@@ -20,6 +20,19 @@ print(bro.endswith('Madhava'))
 
 #String Slicing
 
+a = 'Replica, the solo learner.'
+print(a[:3])
+print(a[-1])
+print(a[:-1])
+print(a[len(a):-1])
+print(a[:len(a)])
+print(a[0:len(a):2]) #Skipping 1 characters from the string
+print(len(a))
+print(a[0:26:3]) #Skipping 2 characters from the string
+print(a[0:25])
+print(a[0:26])
+print(a[::-1]) #Reversing the string
+print(a[-26:-1])
 
 
 
