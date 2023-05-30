97b8f02731de53856ae37073e2f8b9e74e4215c9
Rish <akshaw05@aol.com>
2023-05-30 15:09:43 +0530

    String methods

---
+++ String_Methods.py
@@ -0,0 +1,25 @@
+#In python string can be assigned using single quote
+
+bro = 'Shri Madhava'
+print(bro)
+print("Double Quotes")
+print('Single Quotes')
+
+#String Methods
+
+print(len(bro))
+print(bro.index('a'))
+print(bro.count('a'))
+print(bro.split())
+print(bro.upper())
+print(bro.lower())
+print(bro.startswith('S'))
+print(bro.startswith('Shri'))
+print(bro.endswith('a'))
+print(bro.endswith('Madhava'))
+
+#String Slicing
+
+
+
+
