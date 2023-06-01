ac8afb39deb055ae3767d283f6834f01c56f807f
Rish <akshaw05@aol.com>
2023-06-01 12:50:13 +0530

    Loops and functions added

---
+++ For_loop_in_python.py
@@ -0,0 +1,3 @@
+for i in range(1,20+1):
+    print("I am a south indian movie fan",i)
+    
---
+++ Functions.py
@@ -0,0 +1,11 @@
+#Write a function of sum of two numbers
+
+def sum(a,b):
+    return a+b
+
+print(sum(5,3))
+
+def greetings(R,N):
+    print("Hello",R,"and",N)
+    
+print(greetings("Rahul","Neeraj"))
