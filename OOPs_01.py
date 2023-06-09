adf45bf0df9d01bbc8bf87ff3954479181874383
ak shaw <akshaw05@aol.com>
2023-06-09 12:58:47 +0530

    Objects and Classe

---
+++ OOPs_01.py
@@ -0,0 +1,16 @@
+#How to create a class
+class names:
+    n1 = input("Enter your name :")
+    def n():
+         print("good!")
+
+obj1 = names()
+print(obj1.n1)
+
+def __init__(self,firm_n,Estd):
+         self.firm_n = firm_n
+         self.Estd = Estd
+        
+obj2 = names("REHR",1997)
+print(obj2.firm_n)
+print(obj2.Estd)
