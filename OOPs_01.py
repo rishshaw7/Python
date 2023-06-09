4c8b00c540db28c0da7830481c56ab00ec2607dc
ak shaw <akshaw05@aol.com>
2023-06-09 13:04:33 +0530

    Error Solved

--- OOPs_01.py
+++ OOPs_01.py
@@ -1,13 +1,7 @@
 #How to create a class
 class names:
-    n1 = input("Enter your name :")
-    def n():
-         print("good!")
 
-obj1 = names()
-print(obj1.n1)
-
-def __init__(self,firm_n,Estd):
+    def __init__(self,firm_n,Estd):
          self.firm_n = firm_n
          self.Estd = Estd
         
