b386c4b7c933f6e22cf7c312ee069e7e88a4be71
ak shaw <akshaw05@aol.com>
2023-06-09 13:05:52 +0530

    Error Solved

--- OOPs_01.py
+++ OOPs_01.py
@@ -1,10 +1,10 @@
 #How to create a class
-class names:
+class company:
 
     def __init__(self,firm_n,Estd):
          self.firm_n = firm_n
          self.Estd = Estd
         
-obj2 = names("REHR",1997)
+obj2 = company("REHR",1997)
 print(obj2.firm_n)
 print(obj2.Estd)
