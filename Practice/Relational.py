c4bd186a6c8446de9c3400e6afa8b5898764687e
Rishu Shaw <akshaw05@aol.com>
2023-10-26 20:48:09 +0530

    Directory Changed

---
+++ Practice/Relational.py
@@ -0,0 +1,19 @@
+b = 5>7
+b1=5==5
+b2=5<7
+b3=5>=5
+b4=5>=4
+b5=5<=7
+b6=5<=5
+b7=5!=7
+b8=5!=5
+
+print(b)
+print(b1)
+print(b2)
+print(b3)
+print(b4)
+print(b5)
+print(b6)
+print(b7)
+print(b8)
---
+++ Practice/booleanex.py
@@ -0,0 +1,12 @@
+def nl():
+      print("\n")
+
+
+b1=5+6 == 111
+b2=5*5 == 25
+
+print(b1)
+print(b2)
+print(type(b1),type(b2))
+
+nl()
