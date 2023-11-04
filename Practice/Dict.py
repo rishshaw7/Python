87931b6f04c0cc0c537cab06df5176151a268718
Rishu Shaw <akshaw05@aol.com>
2023-11-04 10:41:03 +0530

    Dictionary Files added

---
+++ Practice/Dict.py
@@ -0,0 +1,22 @@
+bio_data={
+    "Name":"Rishu Shaw",
+    "Age":"17",
+    "D.O.B":"27/10/2006",
+    "Place of Birth":"West Bengal,Hooghly",
+    "Gender":"Male",
+    "Address":"Uttarpara",
+    "Phone Number":"9******62",
+    "Hobby":"Reading Book",
+    "Religion":"Hindu",
+    "Education":"DMA"
+}
+
+#Updating to Bio Data
+bio_data["Address"] = "Hindmotor"
+
+#Adding to Bio Data
+bio_data["Job"] = "Programmer"
+
+#Deleting From Bio Data
+del bio_data["Education"]
+
---
+++ Practice/Formatting.py
@@ -0,0 +1,11 @@
+def p():
+
+    s=input("Enter Your Address:\n")
+    print(f"My address where I live is {s}")
+    print("My address where I live  is {}".format(s))
+p()
+
+
+
+print("My name is Rishu Shaw.")
+p()
